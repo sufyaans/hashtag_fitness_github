c31776aaf063d2deb0d402db53a2ccd676885f81
Sufyaan Seedat <ss1224@student.le.ac.uk>
2021-11-25 16:27:38 +0000

    Navigation bar final

--- lib/home.dart
+++ lib/home.dart
@@ -7,10 +7,10 @@ import 'page/workout.dart';
 
 class Home extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  homeState createState() => homeState();
 }
 
-class _HomeState extends State<Home> {
+class homeState extends State<Home> {
   int currentTab = 0;
   final List<Widget> screens = [
     Exercise(),
@@ -30,14 +30,7 @@ class _HomeState extends State<Home> {
         child: currentScreen,
         bucket: bucket,
       ),
-      // floatingActionButton: FloatingActionButton(
-      //   child: Icon(Icons.add),
-      //   onPressed: () {},
-      // ),
-      //floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        notchMargin: 10,
         child: Container(
           height: 60,
           child: Row(
@@ -48,7 +41,7 @@ class _HomeState extends State<Home> {
                 children: [
                   // Exercise
                   MaterialButton(
-                    minWidth: 40,
+                    //minWidth: 40,
                     onPressed: () {
                       setState(() {
                         currentScreen = Exercise();
@@ -71,84 +64,10 @@ class _HomeState extends State<Home> {
                       ],
                     ),
                   ),
-                  //Measurement
-                  MaterialButton(
-                    minWidth: 21,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = Measurement();
-                        currentTab = 1;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.square_foot, //Fix Icon
-                          color: currentTab == 1 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Measurement',
-                          style: TextStyle(
-                              color:
-                                  currentTab == 1 ? Colors.blue : Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Nutrition
-                  MaterialButton(
-                    minWidth: 21,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = Nutrition();
-                        currentTab = 2;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.restaurant, //Fix icon
-                          color: currentTab == 2 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Nutrition',
-                          style: TextStyle(
-                              color:
-                                  currentTab == 2 ? Colors.blue : Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                  //Progress
-                  MaterialButton(
-                    minWidth: 21,
-                    onPressed: () {
-                      setState(() {
-                        currentScreen = Progress();
-                        currentTab = 3;
-                      });
-                    },
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.pie_chart, //Fix Icon
-                          color: currentTab == 3 ? Colors.blue : Colors.grey,
-                        ),
-                        Text(
-                          'Progress',
-                          style: TextStyle(
-                              color:
-                                  currentTab == 3 ? Colors.blue : Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
+
                   //Workout
                   MaterialButton(
-                    minWidth: 21,
+                    //minWidth: 40,
                     onPressed: () {
                       setState(() {
                         currentScreen = Workout();
@@ -171,8 +90,86 @@ class _HomeState extends State<Home> {
                       ],
                     ),
                   ),
+
+                  //Measurement
+                  // MaterialButton(
+                  //   //minWidth: 40,
+                  //   onPressed: () {
+                  //     setState(() {
+                  //       currentScreen = Measurement();
+                  //       currentTab = 1;
+                  //     });
+                  //   },
+                  //   child: Column(
+                  //     mainAxisAlignment: MainAxisAlignment.center,
+                  //     children: [
+                  //       Icon(
+                  //         Icons.square_foot, //Fix Icon
+                  //         color: currentTab == 1 ? Colors.blue : Colors.grey,
+                  //       ),
+                  //       Text(
+                  //         'Measure',
+                  //         style: TextStyle(
+                  //             color:
+                  //                 currentTab == 1 ? Colors.blue : Colors.grey),
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+
+                  // Nutrition
+                  MaterialButton(
+                    //minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Nutrition();
+                        currentTab = 2;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.restaurant, //Fix icon
+                          color: currentTab == 2 ? Colors.blue : Colors.grey,
+                        ),
+                        Text(
+                          'Nutrition',
+                          style: TextStyle(
+                              color:
+                                  currentTab == 2 ? Colors.blue : Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  //Progress
+                  MaterialButton(
+                    //minWidth: 40,
+                    onPressed: () {
+                      setState(() {
+                        currentScreen = Progress();
+                        currentTab = 3;
+                      });
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.pie_chart, //Fix Icon
+                          color: currentTab == 3 ? Colors.blue : Colors.grey,
+                        ),
+                        Text(
+                          'Progress',
+                          style: TextStyle(
+                              color:
+                                  currentTab == 3 ? Colors.blue : Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
