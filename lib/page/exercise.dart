97b8d95a67840ea140e2f48b1f9e7aa5a9b9e4ae
Sufyaan Seedat <ss1224@student.le.ac.uk>
2021-12-25 16:33:21 +0000

    Comment

--- lib/page/exercise.dart
+++ lib/page/exercise.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:firebase_database/firebase_database.dart';
 
+//Exercise database needs to be added
 class Exercise extends StatefulWidget {
   @override
   _ExerciseState createState() => _ExerciseState();
@@ -18,17 +21,5 @@ class _ExerciseState extends State<Exercise> {
         child: Text('Exercise Screen', style: TextStyle(fontSize: 40)),
       ),
     );
-  }
-}
-
-class listPage extends StatefulWidget {
-  @override
-  _listPageState createState() => _listPageState();
-}
-
-class _listPageState extends State<listPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
