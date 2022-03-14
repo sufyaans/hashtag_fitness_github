417140c05a3a0ec43a4100c82fe5bf08af8fc6cf
sufyaans <ss1224@student.le.ac.uk>
2022-03-14 13:13:34 +0000

    removing unused code

--- lib/page/measurementCalendar.dart
+++ lib/page/measurementCalendar.dart
@@ -1,14 +1,13 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:hashtag_fitness/variables.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'dart:collection';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hashtag_fitness/variables.dart' as vr;
 
-/// Example event class.
+/// Event class.
 class Event {
   final String title;
   const Event(this.title);
@@ -17,30 +16,19 @@ class Event {
   String toString() => title;
 }
 
-/// Example events.
-///
-/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
+// Events
+// Using a LinkedHashMap is highly recommended if you decide to use a map.
 final kEvents = LinkedHashMap<DateTime, List<Event>>(
   equals: isSameDay,
   hashCode: getHashCode,
 )..addAll(_kEventSource);
 final _kEventSource = Map<DateTime, List<Event>>();
-// final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-//     key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-//     value: (item) => List.generate(
-//         item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-//   ..addAll({
-//     kToday: [
-//       Event('Today\'s Event 1'),
-//       Event('Today\'s Event 2'),
-//     ],
-//   });
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
 }
 
-/// Returns a list of [DateTime] objects from [first] to [last], inclusive.
+// Returns a list of DateTime objects from first to last, inclusive.
 List<DateTime> daysInRange(DateTime first, DateTime last) {
   final dayCount = last.difference(first).inDays + 1;
   return List.generate(
@@ -76,20 +64,7 @@ class _CalendarState extends State<measurementCalendar> {
         .doc(FirebaseAuth.instance.currentUser!.uid)
         .collection('Measurements')
         .get();
-    // setState(() {
-    //   _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-    //       key: (item) =>
-    //           DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-    //       value: (item) => List.generate(
-    //           item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-    //     ..addAll({
-    //       kToday: [
-    //         Event('Today\'s Event 1'),
-    //         Event('Today\'s Event 2'),
-    //       ],
-    //     });
-    // });
-    // print(_kEventSource);
+
     // Get data from docs and convert map to List
     final allData = await querySnapshot.docs.map((doc) => doc.data()).toList();
 
--- lib/page/nutritionCalendar.dart
+++ lib/page/nutritionCalendar.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_typing_uninitialized_variables
 
 import 'package:flutter/material.dart';
 import 'package:hashtag_fitness/variables.dart';
@@ -9,7 +9,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:hashtag_fitness/variables.dart' as vr;
 import 'dart:io';
 
-/// Example event class.
+// Event class.
 class Event {
   final String title;
   const Event(this.title);
@@ -18,30 +18,19 @@ class Event {
   String toString() => title;
 }
 
-/// Example events.
-///
-/// Using a [LinkedHashMap] is highly recommended if you decide to use a map.
+// Events
+// Using a LinkedHashMap is highly recommended if you decide to use a map.
 final kEvents = LinkedHashMap<DateTime, List<Event>>(
   equals: isSameDay,
   hashCode: getHashCode,
 )..addAll(_kEventSource);
 final _kEventSource = Map<DateTime, List<Event>>();
-// final _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-//     key: (item) => DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-//     value: (item) => List.generate(
-//         item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-//   ..addAll({
-//     kToday: [
-//       Event('Today\'s Event 1'),
-//       Event('Today\'s Event 2'),
-//     ],
-//   });
 
 int getHashCode(DateTime key) {
   return key.day * 1000000 + key.month * 10000 + key.year;
 }
 
-/// Returns a list of [DateTime] objects from [first] to [last], inclusive.
+// Returns a list of DateTime objects from first to last, inclusive.
 List<DateTime> daysInRange(DateTime first, DateTime last) {
   final dayCount = last.difference(first).inDays + 1;
   return List.generate(
@@ -79,30 +68,11 @@ class _CalendarState extends State<nutritionCalendar> {
         .doc(FirebaseAuth.instance.currentUser!.uid)
         .collection('Nutrition')
         .get();
-    // setState(() {
-    //   _kEventSource = Map.fromIterable(List.generate(50, (index) => index),
-    //       key: (item) =>
-    //           DateTime.utc(kFirstDay.year, kFirstDay.month, item * 5),
-    //       value: (item) => List.generate(
-    //           item % 4 + 1, (index) => Event('Event $item | ${index + 1}')))
-    //     ..addAll({
-    //       kToday: [
-    //         Event('Today\'s Event 1'),
-    //         Event('Today\'s Event 2'),
-    //       ],
-    //     });
-    // });
+
     // print(_kEventSource);
     // Get data from docs and convert map to List
     final allData = await querySnapshot.docs.map((doc) => doc.data()).toList();
     for (var i in allData) {
-      // print(
-      //   (i as Map<String, dynamic>)['timestamp'].toDate().year.toString() +
-      //       (i as Map<String, dynamic>)['timestamp'].toDate().month.tostring() +
-      //       (i as Map<String, dynamic>)['timestamp'].toDate().day.toString() +
-      //       (i as Map<String, dynamic>)['timestamp'].toDate().hour.toString() +
-      //       (i as Map<String, dynamic>)['timestamp'].toDate().minute.toString(),
-      // );
       if (cur == 0) {
         cur = (i as Map<String, dynamic>)['timestamp'].toDate().day;
       }
@@ -306,7 +276,6 @@ class _CalendarState extends State<nutritionCalendar> {
                         } on Exception catch (e) {
                           return Container();
                         }
-                        ;
                       },
                     ),
                   ),
