333da9fff4a5c10b0d5f609db1b90c6972c3e7c8
sufyaans <ss1224@student.le.ac.uk>
2022-03-23 13:04:53 +0000

    Displaying saved workouts (Error with dependency)

--- lib/page/workout.dart
+++ lib/page/workout.dart
@@ -1,9 +1,12 @@
-// ignore_for_file: deprecated_member_use, prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace
+// ignore_for_file: deprecated_member_use, prefer_const_constructors, use_key_in_widget_constructors, sized_box_for_whitespace, library_prefixes
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bounceable/flutter_bounceable.dart';
 import 'package:hashtag_fitness/page/createWorkout.dart';
 import 'package:hashtag_fitness/variables.dart' as vr;
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:slide_popup_dialog/slide_popup_dialog.dart' as slideDialog;
 
 class Workout extends StatefulWidget {
   @override
@@ -42,6 +45,98 @@ class WorkoutPage extends StatefulWidget {
 }
 
 class _WorkoutPageState extends State<WorkoutPage> {
+  var workouts = [];
+  initState() {
+    getData();
+  }
+
+  CollectionReference _collectionRef = FirebaseFirestore.instance
+      .collection('users')
+      .doc(FirebaseAuth.instance.currentUser!.uid)
+      .collection('WorkoutTemplates');
+
+  getData() async {
+    // Get docs from collection reference
+    QuerySnapshot querySnapshot = await _collectionRef.get();
+
+    // Get data from docs and convert map to List
+    final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+    setState(() {
+      workouts = allData;
+    });
+    print(allData);
+  }
+
+  _showDialog(var i) {
+    slideDialog.showSlideDialog(
+      context: context,
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          ListView.builder(
+              itemCount: workouts[i]['workoutList'].length,
+              shrinkWrap: true,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  // leading: Icon(Icons.list),
+                  trailing: Padding(
+                      padding: EdgeInsetsDirectional.only(start: 10),
+                      child: Text(
+                        workouts[i]["sets"][index],
+                        style: TextStyle(
+                          fontFamily: vr.basicFont,
+                          fontSize: 18,
+                          color: vr.orangeColor,
+                        ),
+                      )),
+                  title: Padding(
+                      padding: EdgeInsetsDirectional.only(end: 10),
+                      child: Text(
+                        workouts[i]["workoutList"][index],
+                        style: TextStyle(
+                          fontFamily: vr.basicFont,
+                          fontSize: 18,
+                          color: vr.whiteColor,
+                        ),
+                      )),
+                );
+              }),
+          Bounceable(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => CreateWorkoutScreen(),
+                ),
+              );
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              height: 40,
+              child: Material(
+                borderRadius: BorderRadius.circular(24),
+                color: vr.orangeColor,
+                elevation: 7,
+                child: Center(
+                  child: Text(
+                    'START WORKOUT',
+                    style: TextStyle(
+                      color: vr.whiteColor,
+                      fontFamily: vr.basicFont,
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      barrierColor: Colors.white.withOpacity(0.7),
+      pillColor: Colors.white,
+      backgroundColor: Colors.black,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -88,6 +183,31 @@ class _WorkoutPageState extends State<WorkoutPage> {
                 color: vr.whiteColor,
               ),
             ),
+            ListView.builder(
+                shrinkWrap: true,
+                itemCount: workouts.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Bounceable(
+                      onTap: () {
+                        _showDialog(index);
+                      },
+                      child: ListTile(
+                          leading: Icon(Icons.list, color: Colors.white),
+                          // trailing: Text(
+                          //   workouts[index]["name"],
+                          //   style: TextStyle(
+                          //     fontFamily: vr.basicFont,
+                          //     fontSize: 18,
+                          //     color: vr.whiteColor,
+                          //   ),
+                          // ),
+                          title: Text(workouts[index]["name"],
+                              style: TextStyle(
+                                fontFamily: vr.basicFont,
+                                fontSize: 18,
+                                color: vr.whiteColor,
+                              ))));
+                }),
           ],
         ),
       ),
