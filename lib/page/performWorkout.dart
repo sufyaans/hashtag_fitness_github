72672289e6bc60435fc1153ceede3240a625e01d
sufyaans <ss1224@student.le.ac.uk>
2022-04-15 13:04:31 +0100

    small changes to code

--- lib/page/performWorkout.dart
+++ lib/page/performWorkout.dart
@@ -87,7 +87,7 @@ class _PerformWorkoutPageState extends State<PerformWorkoutPage> {
       padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(25.0),
-        color: vr.orangeColor,
+        color: vr.whiteColor,
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
